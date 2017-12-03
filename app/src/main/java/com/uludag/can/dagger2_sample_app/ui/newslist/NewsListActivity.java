4dd0cfbc945bb792896db719c1666b7da40fdde6
canuludag <cucanuludag@gmail.com>
2017-12-03 21:04:12 +0300

    refactor: Setup package structure as package by feature

---
+++ app/src/main/java/com/uludag/can/dagger2_sample_app/ui/newslist/NewsListActivity.java
@@ -0,0 +1,15 @@
+package com.uludag.can.dagger2_sample_app.ui.newslist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.uludag.can.dagger2_sample_app.R;
+
+public class NewsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_list);
+    }
+}
