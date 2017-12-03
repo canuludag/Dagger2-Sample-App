b84d6b95ab7b55b6abc1c13ef03c4a71c69c49ae
canuludag <cucanuludag@gmail.com>
2017-12-03 22:28:51 +0300

    feat: Implement layout design for NewsListActivity

--- app/src/main/java/com/uludag/can/dagger2_sample_app/ui/newslist/NewsListActivity.java
+++ app/src/main/java/com/uludag/can/dagger2_sample_app/ui/newslist/NewsListActivity.java
@@ -1,9 +1,10 @@
 package com.uludag.can.dagger2_sample_app.ui.newslist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.uludag.can.dagger2_sample_app.R;
+
 
 public class NewsListActivity extends AppCompatActivity {
 
