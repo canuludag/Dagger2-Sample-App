a558b88770cc052d7235bf5d759a9e4116fd9c38
canuludag <cucanuludag@gmail.com>
2017-12-03 22:57:58 +0300

    feat: Implement LoginActivity layout design

---
+++ app/src/main/java/com/uludag/can/dagger2_sample_app/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.uludag.can.dagger2_sample_app.ui.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.uludag.can.dagger2_sample_app.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
